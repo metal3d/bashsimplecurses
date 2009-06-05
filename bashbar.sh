26cd36679db2ad1a54b64c7bb0eb031aec7b8ef8
Metal3d <Metal3d@5ebbb9c8-51aa-11de-b56c-cbe681ab91ab>
2009-06-05 15:14:58 +0000

    Some comments and usage of append_file function

--- bashbar.sh
+++ bashbar.sh
@@ -3,6 +3,7 @@
 . `dirname $0`/simple_curses.sh
 
 main (){
+    #basic informations, hostname, date,...
     window "`hostname`" "red"
     append "`date`"
     addsep
@@ -11,25 +12,27 @@ main (){
     append_tabbed "`awk '{print "Load average:" $1 " " $2 " " $3}' < /proc/loadavg`" 2
     endwin 
     
+    #memory usage
     window "Memory usage" "red"
     append_tabbed `cat /proc/meminfo | awk '/MemTotal/ {print "Total:" $2/1024}'` 2
     append_tabbed `cat /proc/meminfo | awk '/MemFree/ {print "Used:" $2/1024}'` 2
     endwin
 
+    #5 more used process ordered by cpu and memory usage
     window "Processus taking memory and CPU" "green"
     for i in `seq 2 6`; do
         append_tabbed "`ps ax -o pid,rss,pcpu,ucmd --sort=-cpu,-rss | sed -n "$i,$i p" | awk '{printf "%s: %smo:  %s%%" , $4, $2/1024, $3 }'`" 3
     done
     endwin
 
+    #get dmesg, log it then send to deskbar
     window "Last kernel messages" "blue"
-    dmesg | tail -n 10 > /tmp/deskbar.dmesg
-    while read line; do
-        append_tabbed "$line" 1 "~"
-    done < /tmp/deskbar.dmesg
-    rm -f /tmp/deskbar.dmesg
+    dmesg | tail -n 10 > /dev/shm/deskbar.dmesg
+    append_file /dev/shm/deskbar.dmesg
+    rm -f /dev/shm/deskbar.dmesg
     endwin
 
+    #a special manipulation to get net interfaces  and IP
     window "Inet interfaces" "grey"
     _ifaces=$(for inet in `ifconfig | cut -f1 -d " " | sed -n "/./ p"`; do ifconfig $inet | awk 'BEGIN{printf "%s", "'"$inet"'"} /adr:/ {printf ":%s\n", $2}'|sed 's/adr://'; done)
     for ifac in $_ifaces; do
@@ -37,4 +40,4 @@ main (){
     done
     endwin
 }
-main_loop 1
+main_loop 0.5
