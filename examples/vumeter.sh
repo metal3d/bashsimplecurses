6f5a41e9883cb4c5c5f40c6742d611d184eec4b2
Andrzej Bednarski <andrzej.bednarski@gmail.com>
2019-02-09 20:49:00 +0100

    Minor corrections and documentation
    - Correct some spelling issues
    - Remove unnecessary spaces and tabulations
    - Set executable flag on examples/bashbar2.sh file
    - Update vumeter and progress documentation (copy paste error)
    - Add short comments for example code
    - Resolve path to tux.gif if run from different location

--- examples/vumeter.sh
+++ examples/vumeter.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
-#   ./choice "choice1" "choice2" "choice3"
-#   returns 255 if none chosen
-#   returns number of choice if one chosen
-#   
+#   ./vumeter.sh
+#   This examples shows two components: a vumeter and a blinkenlights.
+#   The vumeter shows a constant value, while the blinkenlights show variable
+#   status of each light until the progress exceed maxprogress, then returns 255
+#
 source $(dirname $0)/../simple_curses.sh
 progress=0
 maxprogress=97
 main(){
     window "waiting for bar to move" "red" "33%" "blue"
     vumeter "RVol" "15" "25" "30" "green" "red" "gray"
-    blinkenlights "Blinken" "green" "red" "gray" "black" $((progress % 2 )) $((progress % 3 )) $((progress % 5 )) $((progress % 7 )) $((progress % 9 )) $((progress % 6 ))   
+    blinkenlights "Blinken" "green" "red" "gray" "black" $((progress % 2 )) $((progress % 3 )) $((progress % 5 )) $((progress % 7 )) $((progress % 9 )) $((progress % 6 ))
     endwin
 }
 update(){
